7d882fdb2bd697d84ae43cad76eda5a77520cc50
Afliex <kelechistone123@gmail.com>
2022-05-15 11:52:22 +0100

    Added an Infinity Orb Item.

--- src/main/java/net/afliex/infinitymod/InfinityMod.java
+++ src/main/java/net/afliex/infinitymod/InfinityMod.java
@@ -1,15 +1,18 @@
 package net.afliex.infinitymod;
 
+import net.afliex.infinitymod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class InfinityMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "infinitymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+
 	}
 }
---
+++ src/main/java/net/afliex/infinitymod/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.afliex.infinitymod.item;
+
+import net.afliex.infinitymod.InfinityMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+    public static final Item INFINITY_ORB = registerItem("infinity_orb",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(InfinityMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        InfinityMod.LOGGER.info("Registering Items For " + InfinityMod.MOD_ID);
+    }
+
+}
