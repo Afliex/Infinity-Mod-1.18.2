5e3d04b39c599a9d0c78f794e78eec427eb370c7
Afliex <kelechistone123@gmail.com>
2022-05-15 12:50:19 +0100

    Added an infinity block.

--- src/main/java/net/afliex/infinitymod/InfinityMod.java
+++ src/main/java/net/afliex/infinitymod/InfinityMod.java
@@ -1,5 +1,6 @@
 package net.afliex.infinitymod;
 
+import net.afliex.infinitymod.block.ModBlocks;
 import net.afliex.infinitymod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -13,6 +14,7 @@ public class InfinityMod implements ModInitializer {
 	public void onInitialize() {
 
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 
 	}
 }
